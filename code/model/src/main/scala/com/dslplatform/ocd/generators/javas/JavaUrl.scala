41501c5f59e83fe1dbfc2ccd39901290456cac7c
Marko Elezovic <marko@element.hr>
2014-03-22 17:49:24 +0100

    Added Java TypeFactory (utilities which are candidates for inclusion in the client library) Currently used to construct URI & IP types.

--- code/model/src/main/scala/com/dslplatform/ocd/generators/javas/JavaUrl.scala
+++ code/model/src/main/scala/com/dslplatform/ocd/generators/javas/JavaUrl.scala
@@ -9,25 +9,16 @@ object JavaUrl
 
   val classReference = "java.net.URI"
 
-  val defaultSingle = E"""com.dslplatform.ocd.tools.URIFactory.createUri("http://127.0.0.1")"""
+  val defaultSingle = E"""com.dslplatform.TypeFactory.buildURI("http://127.0.0.1")"""
 
   val nonDefaultValues = Seq(
-
-      /* http */
-        E"""com.dslplatform.ocd.tools.URIFactory.createUri("http://www.xyz.com")"""
-      /* https */
-      , E"""com.dslplatform.ocd.tools.URIFactory.createUri("https://www.abc.com")"""
-      /* ftp */
-      , E"""com.dslplatform.ocd.tools.URIFactory.createUri("ftp://www.pqr.com")"""
-      /* localhost */
-      , E"""com.dslplatform.ocd.tools.URIFactory.createUri("https://localhost:8080")"""
-      /* mailto */
-      , E"""com.dslplatform.ocd.tools.URIFactory.createUri("mailto:snail@mail.hu")"""
-      /* file */
-      , E"""com.dslplatform.ocd.tools.URIFactory.createUri("file://~/opt/somefile.md")"""
-      /* tcp */
-      , E"""com.dslplatform.ocd.tools.URIFactory.createUri("tcp://localhost:8181")"""
-      /* complex */
-      , E"""com.dslplatform.ocd.tools.URIFactory.createUri("failover:(tcp://localhost:8181,tcp://localhost:8080)")"""
+    E"""com.dslplatform.TypeFactory.buildURI("http://www.xyz.com")"""
+  , E"""com.dslplatform.TypeFactory.buildURI("https://www.abc.com")"""
+  , E"""com.dslplatform.TypeFactory.buildURI("ftp://www.pqr.com")"""
+  , E"""com.dslplatform.TypeFactory.buildURI("https://localhost:8080")"""
+  , E"""com.dslplatform.TypeFactory.buildURI("mailto:snail@mail.hu")"""
+  , E"""com.dslplatform.TypeFactory.buildURI("file:///~/opt/somefile.md")"""
+  , E"""com.dslplatform.TypeFactory.buildURI("tcp://localhost:8181")"""
+  , E"""com.dslplatform.TypeFactory.buildURI("failover:(tcp://localhost:8181,tcp://localhost:8080)")"""
   )
 }

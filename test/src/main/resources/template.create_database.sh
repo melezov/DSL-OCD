4fd45a1881bd619d8f531631502616a25d9aba0a
hperadin <hperadin@element.hr>
2014-12-08 11:00:38 +0100

    Delete old template scripts

--- test/src/main/resources/template.create_database.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-DATABASE_NAME=${dbName}
-DATABASE_USER=${dbUser}
-DATABASE_PASSWORD=${dbPassword}
-
-echo
-echo "Creating the database..."
-createdb -U postgres $DATABASE_NAME -O $DATABASE_USER

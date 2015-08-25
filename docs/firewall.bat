20dada7541db55cfa7feda09483322c8796d6437
Tomo Ferega <tomo@element.hr>
2015-08-25 16:08:25 +0200

    yhyhj

--- docs/firewall.bat
+++ docs/firewall.bat
@@ -3,8 +3,8 @@ pushd "%~dp0"
 
 >firewall-rule.bat cd.
 for /l %%p in (10000,1,10400) do (
-  >>firewall-rule.bat echo netsh http add urlacl url=http://[::1]:%%p/ user=%COMPUTERNAME%\%USERNAME%
-  >>firewall-rule.bat echo netsh http add urlacl url=http://127.0.0.1:%%p/ user=%COMPUTERNAME%\%USERNAME%
-  >>firewall-rule.bat echo netsh http add urlacl url=http://localhost:%%p/ user=%COMPUTERNAME%\%USERNAME%
+  >>firewall-rule.bat echo netsh http add urlacl "url=http://[::1]:%%p/" "user=%COMPUTERNAME%\%USERNAME%"
+  >>firewall-rule.bat echo netsh http add urlacl "url=http://127.0.0.1:%%p/" "user=%COMPUTERNAME%\%USERNAME%"
+  >>firewall-rule.bat echo netsh http add urlacl "url=http://localhost:%%p/" "user=%COMPUTERNAME%\%USERNAME%"
 )
 popd

e524dc87c2ac3cc12e93e4dcff8ddf147f681af4
Marko Elezovic <marko@element.hr>
2015-08-27 17:02:59 +0200

    Generalize generator generator. Sanity check in run-ocd.

--- docs/generate-ocd-generator.bat
+++ docs/generate-ocd-generator.bat
@@ -1,63 +1,59 @@
 @echo off
-pushd "D:\Code"
+setlocal
 
-if exist "C:\Users\Marko\.ivy2\cache\com.dslplatform.ocd" rmdir /S /Q "C:\Users\Marko\.ivy2\cache\com.dslplatform.ocd"
-if exist "C:\Users\Marko\.ivy2\local\com.dslplatform.ocd" rmdir /S /Q "C:\Users\Marko\.ivy2\local\com.dslplatform.ocd"
+if exist "%USERPROFILE%\.ivy2\cache\com.dslplatform.ocd" rmdir /S /Q "%USERPROFILE%\.ivy2\cache\com.dslplatform.ocd"
+if exist "%USERPROFILE%\.ivy2\local\com.dslplatform.ocd" rmdir /S /Q "%USERPROFILE%\.ivy2\local\com.dslplatform.ocd"
 
 echo ### ABOUT TO RESET DSL-OCD (!HARD!) ###
 pause
 echo ### ARE YOU REALLY, REALLY SURE ??? ###
 pause
 
-git reset --hard
-git clean -d -f
+set OCD=%~dp0\DSL-OCD
+cd /D "%OCD%"
 
-:: cd "D:\Code\DSL-OCD\util\generator"
+call git reset --hard
+call git clean -d -f
+
+:: cd "%OCD%\util\testing"
 :: call sbt clean update eclipse publishLocal publish
+:: copy /Y "%OCD%\util\testing\target\dsl-ocd-util-testing-0.1.0.jar" "%OCD%\test\src\main\resources\template.tools\lib"
 
-:: cd "D:\Code\DSL-OCD\util\testing"
-:: call sbt clean update eclipse publishLocal publish
-:: copy /Y "D:\Code\DSL-OCD\util\testing\target\dsl-ocd-util-testing-0.1.0.jar" "D:\Code\DSL-OCD\test\src\main\resources\template.tools\lib"
-
-:: cd "D:\Code\DSL-OCD\util\report"
+:: cd "%OCD%\util\report"
 :: call mvn clean package
-:: copy /Y "D:\Code\DSL-OCD\util\report\target\ocd-report-0.1.0-jar-with-dependencies.jar" "D:\Code\DSL-OCD\test\src\main\resources\template.tools\build\ocd-report-0.1.0.jar"
+:: copy /Y "%OCD%\util\report\target\ocd-report-0.1.0-jar-with-dependencies.jar" "%OCD%\test\src\main\resources\template.tools\build\ocd-report-0.1.0.jar"
 
-:: cd "D:\Code\DSL-OCD\util\ping"
+:: cd "%OCD%\util\ping"
 :: call sbt clean update eclipse publishLocal publish
-:: copy /Y "D:\Code\DSL-OCD\util\ping\target\dsl-ocd-util-ping-0.1.0.jar" "D:\Code\DSL-OCD\test\src\main\resources\template.tools\build"
+:: copy /Y "%OCD%\util\ping\target\dsl-ocd-util-ping-0.1.0.jar" "%OCD%\test\src\main\resources\template.tools\build"
 
-cd "D:\Code\DSL-OCD\model\generator"
+cd "%OCD%\model\generator"
 call sbt clean update eclipse run
 
-cd "D:\Code\DSL-OCD\model\kinds"
+cd "%OCD%\model\kinds"
 call sbt clean update eclipse publishLocal publish
 
-cd "D:\Code\DSL-OCD\model\boxes"
+cd "%OCD%\model\boxes"
 call sbt clean update eclipse publishLocal publish
 
-cd "D:\Code\DSL-OCD\model\types"
+cd "%OCD%\model\types"
 call sbt clean update eclipse publishLocal publish
 
-cd "D:\Code\DSL-OCD\model\dsls"
+cd "%OCD%\model\dsls"
 call sbt clean update eclipse publishLocal publish
 
-cd "D:\Code\DSL-OCD\model\javas"
+cd "%OCD%\model\javas"
 call sbt clean update eclipse publishLocal publish
 
-cd "D:\Code\DSL-OCD\model\java-asserts"
+cd "%OCD%\model\java-asserts"
 call sbt clean update eclipse publishLocal publish
-copy /Y "d:\Code\DSL-OCD\model\java-asserts\target\dsl-ocd-model-java-asserts-0.1.0-SNAPSHOT.jar" "D:\Code\DSL-OCD\test\src\main\resources\template.tools\lib"
+copy /Y "%OCD%\model\java-asserts\target\dsl-ocd-model-java-asserts-0.1.0-SNAPSHOT.jar" "%OCD%\test\src\main\resources\template.tools\lib"
 
-cd "D:\Code\DSL-OCD\model\scalas"
+cd "%OCD%\model\scalas"
 call sbt clean update eclipse publishLocal publish
 
-cd "D:\Code\DSL-OCD\test"
+cd "%OCD%\test"
 call sbt clean update eclipse run
-
-cd "D:\Code\DSL-OCD-Target"
-call ant all report
-report\site\index.html
 
 popd
 goto :EOF

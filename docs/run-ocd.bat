cddbba3f18f45958d6df4fd992a8d585ddff24dd
Marko Elezovic <marko@element.hr>
2015-08-27 12:41:22 +0200

    If DSL-OCD-SSH folder exists, use that instead of manual authentication.

--- docs/run-ocd.bat
+++ docs/run-ocd.bat
@@ -1,14 +1,27 @@
 @echo off
+setlocal
 pushd "%~dp0"
 
 if exist DSL-Trash rmdir /S /Q DSL-Trash
 if exist "%USERPROFILE%\.ivy2\cache\com.dslplatform.ocd" move "%USERPROFILE%\.ivy2\cache\com.dslplatform.ocd" DSL-Trash
+if exist "%USERPROFILE%\.ivy2\local\com.dslplatform.ocd" move "%USERPROFILE%\.ivy2\local\com.dslplatform.ocd" DSL-Trash
+
 if exist DSL-OCD move DSL-OCD DSL-Trash
 if exist DSL-OCD-Target move DSL-OCD-Target DSL-Trash
 
 start /MIN cmd /c rmdir /S /Q DSL-Trash
 
+if exist DSL-OCD-SSH goto :ssh
 git clone --depth 1 https://melezov@github.com/element-doo/DSL-OCD.git
+goto :cloned
+
+:ssh
+set DSL_SSH_OLD_HOME=%HOME%
+set HOME=DSL-OCD-SSH
+git clone --depth 1 ssh://git@github.com/element-doo/DSL-OCD.git
+set HOME=%DSL_SSH_OLD_HOME%
+
+:cloned
 cd DSL-OCD\test
 call sbt run
 

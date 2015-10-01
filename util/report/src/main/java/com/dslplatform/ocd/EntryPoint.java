ee694335b78faaf1d5b6ca01ca2bdbc88f3bc404
Tomo Ferega <tomo@element.hr>
2015-10-01 04:22:12 +0200

    Reports now report skipped tests (due to compile errors)

---
+++ util/report/src/main/java/com/dslplatform/ocd/EntryPoint.java
@@ -0,0 +1,29 @@
+package com.dslplatform.ocd;
+
+public class EntryPoint {
+  public static void main(final String[] args) throws Exception {
+    if(args.length != 3 && args.length != 4){
+        System.out.println();
+        System.out.println("Example usage:");
+        System.out.println("java -jar ocd-report-generator.jar root site OCD-Tests-Summary.xml [generate-wins-pages]");
+    } else {
+      runOcdReportGenerator(args[1], args[2], args.length == 4);
+      runSkippedReportGenerator(args[0], args[1]);
+    }
+  }
+  
+  private static void runOcdReportGenerator(
+      final String outputDirectory,
+      final String inputFile,
+      final boolean generateWinsPages) {
+    final OcdReportGenerator generator = new OcdReportGenerator(outputDirectory, inputFile, generateWinsPages);
+    generator.generate();
+  }
+  
+  private static void runSkippedReportGenerator(
+      final String projectDirectory,
+      final String outputDirectory) {
+    final SkippedReportGenerator generator = new SkippedReportGenerator(projectDirectory, outputDirectory);
+    generator.generate();
+  }
+}

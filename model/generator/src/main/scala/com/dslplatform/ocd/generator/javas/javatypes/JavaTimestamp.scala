6a914453022e52c54f55dde20e2aaf22c4ffa551
Marko Elezovic <marko@mentat-labs.com>
2016-04-17 08:20:38 +0200

    Added more use-case Timestamps (different timezones)

--- model/generator/src/main/scala/com/dslplatform/ocd/generator/javas/javatypes/JavaTimestamp.scala
+++ model/generator/src/main/scala/com/dslplatform/ocd/generator/javas/javatypes/JavaTimestamp.scala
@@ -13,9 +13,18 @@ object JavaTimestamp
 
   val defaultSingle: TestValue = "org.joda.time.DateTime.now()" ~ Unstable
 
+  private[this] val UTC = """org.joda.time.DateTimeZone.UTC"""
+  private[this] val GMT = """org.joda.time.DateTimeZone.forID("GMT")"""
+  private[this] val CET = """org.joda.time.DateTimeZone.forID("CET")"""
+  private[this] val EST = """org.joda.time.DateTimeZone.forID("EST")"""
+
   val nonDefaultValues: Seq[TestValue] = Seq(
-    "new org.joda.time.DateTime(0)"                                             // Epoch 1970
-  , "new org.joda.time.DateTime(1, 1, 1, 0, 0, org.joda.time.DateTimeZone.UTC)" // 0001-01-01T00:00:00Z default value for DateTime deserialization on .NET
-  , "new org.joda.time.DateTime(Integer.MAX_VALUE * 1001L)"                     // beyond UNIX epoch (>2038)
+    s"new org.joda.time.DateTime(0, ${GMT})"                                        // Epoch 1970
+  , s"new org.joda.time.DateTime(1, 1, 1, 0, 0, ${UTC})"                            // 0001-01-01T00:00:00Z default value for DateTime deserialization on .NET
+  , s"new org.joda.time.DateTime(Integer.MAX_VALUE * 1001L, ${EST})"                // beyond UNIX epoch (>2038)
+  , s"new org.joda.time.DateTime(2000, 3, 26, 1, 59, 59, ${CET})"                   // before DST
+  , s"new org.joda.time.DateTime(2000, 3, 26, 3, 0, 0, ${CET})"                     // DST start
+  , s"new org.joda.time.DateTime(2000, 10, 29, 0, 59, 59, ${UTC}).withZone(${CET})" // during DST
+  , s"new org.joda.time.DateTime(2000, 10, 29, 1, 0, 0, ${UTC}).withZone(${CET})"   // DST stop
   )
 }

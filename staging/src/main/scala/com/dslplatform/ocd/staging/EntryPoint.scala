cf5a280cea30b01faa9da02da4b8bf411e84a76d
Marko Elezovic <marko.elezovic@oradian.com>
2016-08-16 01:05:43 +0200

    Added revenj.scala (revenj-akka) Bumped everything to latest version

--- staging/src/main/scala/com/dslplatform/ocd/staging/EntryPoint.scala
+++ staging/src/main/scala/com/dslplatform/ocd/staging/EntryPoint.scala
@@ -4,9 +4,21 @@ package staging
 object EntryPoint
     extends App {
 
+  logger.info("#" * 50)
   Source()
+
+  logger.info("#" * 50)
+  scala.io.StdIn.readLine("Inspect versions of newly cloned repositories, then press ENTER to compile")
   Analyse()
+
+  logger.info("#" * 50)
   Compile()
+
+  logger.info("#" * 50)
   Download()
+
+  logger.info("#" * 50)
   Gather()
+
+  sys.exit(0)
 }
